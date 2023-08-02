bb3fef491a2bd51e5c1e7e4e835e0396ace056f4
Vivek Shukla <vivekshuklahere@gmail.com>
2023-08-02 21:37:06 +0530

    Added Docgen

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,7 @@ const { createTokenUser, attachCookiesToResponse } = require("../utils");
 const getSingleUser = async (req, res) => {
   const user = await User.findOne({ _id: req.params.id }).select("-password");
   if (!user) {
-    throw new CustomError.NotFoundError(`No user with id ${req.params.id}`);
+    throw new CustomError.NotFoundError(`No user found with id ${req.params.id}`);
   }
   res.status(StatusCodes.OK).json({ user });
 };
